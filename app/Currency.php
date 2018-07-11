061af0db4bdce34286137b931c55f2d6cadf0d26
pavlo <pavelnemoi@gmail.com>
2018-07-11 13:59:36 +0300

    Added model

---
+++ app/Currency.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    protected $table = 'currency';
+
+    protected $fillable = [
+
+    ];
+}
